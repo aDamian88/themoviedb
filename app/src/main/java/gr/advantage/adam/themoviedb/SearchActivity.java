0d833cfbb61a7bd34ca7da1615892cabf87647df
aDamian88 <adam23@hotmail.gr>
2019-10-16 06:52:27 +0300

    Rename the MainActivity to SearchActivity

--- app/src/main/java/gr/advantage/adam/themoviedb/SearchActivity.java
+++ app/src/main/java/gr/advantage/adam/themoviedb/SearchActivity.java
@@ -3,7 +3,7 @@ package gr.advantage.adam.themoviedb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
