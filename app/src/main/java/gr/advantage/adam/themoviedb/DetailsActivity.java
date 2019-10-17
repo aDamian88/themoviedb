b8cb8294114b2c19832c04e937fc8c284d19db53
aDamian88 <adam23@hotmail.gr>
2019-10-17 09:43:15 +0300

    Showing the title of the chosen movie at a simple "Toast" message.

--- app/src/main/java/gr/advantage/adam/themoviedb/DetailsActivity.java
+++ app/src/main/java/gr/advantage/adam/themoviedb/DetailsActivity.java
@@ -1,7 +1,9 @@
 package gr.advantage.adam.themoviedb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+        String title = (String) intent.getSerializableExtra("title");
+        Toast.makeText(this,title,Toast.LENGTH_SHORT).show();
     }
 }
