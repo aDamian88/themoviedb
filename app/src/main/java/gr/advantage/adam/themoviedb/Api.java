b617e754cc8d95cb73180a1351c5c35f7addfef2
aDamian88 <adam23@hotmail.gr>
2019-10-18 10:20:05 +0300

    Insertion of auth key value at Api interface.

--- app/src/main/java/gr/advantage/adam/themoviedb/Api.java
+++ app/src/main/java/gr/advantage/adam/themoviedb/Api.java
@@ -7,7 +7,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Url;
 
 public interface Api {
-    String BASE_URL = "https://api.themoviedb.org/";
+    String BASE_URL = "https://api.themoviedb.org/3/";
+    String AUTH_KEY = "6b2e856adafcc7be98bdf0d8b076851c";
 
     @GET
     Call<JsonObject> getSearchResult(@Url String url);
