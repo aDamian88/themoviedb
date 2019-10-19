094b9575b9b3b6bc6a8e8b11d700f8e06b562afb
aDamian88 <adam23@hotmail.gr>
2019-10-19 17:15:38 +0300

    Insertion of POSTER_URL variable at Api interface to use it from different classes.

--- app/src/main/java/gr/advantage/adam/themoviedb/Api.java
+++ app/src/main/java/gr/advantage/adam/themoviedb/Api.java
@@ -9,6 +9,8 @@ import retrofit2.http.Url;
 public interface Api {
     String BASE_URL = "https://api.themoviedb.org/3/";
     String AUTH_KEY = "6b2e856adafcc7be98bdf0d8b076851c";
+    String POSTER_URL = "http://image.tmdb.org/t/p/w185/";
+
 
     @GET
     Call<JsonObject> getSearchResult(@Url String url);
